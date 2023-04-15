0a190b3ea0930477a9b5d7eeb5ecabdacdcbba54
SoftEngMatthew <mateus.fonto@gmail.com>
2023-04-15 20:00:54 -0300

    all CRUD functionalities added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,7 +4,7 @@
 import { db } from "../db.js";
 
 // we define the getUsers function
-export const getUsers = (req, res) => {
+const getUsers = (req, res) => {
   // we define the sql query
   const sql = "SELECT * FROM users";
 
@@ -17,3 +17,56 @@ export const getUsers = (req, res) => {
     return res.status(200).json(result);
   });
 };
+
+// addUser
+const addUser = (req, res) => {
+  // we define the sql query with the ? to be replaced by the values
+  const sql =
+    "INSERT INTO users(`name`, `email`, `phone`, `birth_date`) VALUES(?)";
+
+  // destructure the req.body object
+  const { name, email, phone, birth_date } = req.body;
+
+  // we define the values to be inserted into the sql query
+  const values = [name, email, phone, birth_date];
+
+  // we call the db.query function
+  db.query(sql, [values], (err) => {
+    if (err) return res.json({ error: err.message, sql: err.sql });
+
+    return res.status(200).json("User added successfully");
+  });
+};
+
+// updateUser
+const updateUser = (req, res) => {
+  const sql =
+    "UPDATE users SET `name` = ?, `email` = ?, `phone` = ?, `birth_date` = ? WHERE `id` = ?";
+
+  // destructure the req.body object
+  const { name, email, phone, birth_date } = req.body;
+
+  // we define the values to be inserted into the sql query
+  const values = [name, email, phone, birth_date];
+
+  // we call the db.query function
+  db.query(sql, [...values, req.params.id], (err) => {
+    if (err) return res.json({ error: err.message, sql: err.sql });
+
+    return res.status(200).json("User updated successfully");
+  });
+};
+
+// deleteUser
+const deleteUser = (req, res) => {
+  const sql = "DELETE FROM users WHERE `id` = ?";
+
+  // we call the db.query function
+  db.query(sql, [req.params.id], (err) => {
+    if (err) return res.json({ error: err.message, sql: err.sql });
+
+    return res.status(200).json("User deleted successfully");
+  });
+};
+
+export { getUsers, addUser, updateUser, deleteUser };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,19 @@
 // routes = where we define the routes for our application
 import express from "express";
-import { getUsers } from "../controllers/user.js";
+import {
+  getUsers,
+  addUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.js";
 
 // Create a new router instance
 const router = express.Router();
 
 // when we hit the route /users, we want to call the getUsers function
 router.get("/", getUsers);
+router.post("/", addUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
